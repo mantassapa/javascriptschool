70110cba5fb910fb941381b8e8e21d0dc7f619ad
Moksleivis <Moksleivis@kitm.local>
2023-11-08 13:15:42 +0200

    first

---
+++ naujas/index.js
@@ -0,0 +1,25 @@
+"use strict"
+console.log("hello world!")
+var name = "Jonas";
+console.log(name)
+var name = "Petras";
+console.log(name)
+let name1 = "Stasius";
+console.log(name1)
+name1 = "kaska"
+console.log(name1)
+const Name = "Laura";
+console.log(Name)
+
+let vardas;
+vardas = "Sima";
+console.log(vardas)
+
+
+console.log('hello worldd');
+const languoge = 'js';
+const author = 'Eich';
+console.log(languoge + ',' + 'was created by' + ',' + author)
+console.log(`${languoge}, was created by, ${author}`)
+
+const sk = 21;
