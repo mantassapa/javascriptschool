f0b99ccce1cb882dfeb80df28ed7d20645691218
Moksleivis <Moksleivis@kitm.local>
2023-11-08 13:43:21 +0200

    sec

--- naujas/index.js
+++ naujas/index.js
@@ -20,6 +20,11 @@ console.log('hello worldd');
 const languoge = 'js';
 const author = 'Eich';
 console.log(languoge + ',' + 'was created by' + ',' + author)
-console.log(`${languoge}, was created by, ${author}`)
-
+console.log(`                                                       ${languoge}, was created by, ${author}`)
+ 
 const sk = 21;
+
+let myString = "vakariene";
+console.log(myString.charAt(0))
+let myStrin = "vakariene";
+console.log(myStrin.concat(myString,vardas,))
