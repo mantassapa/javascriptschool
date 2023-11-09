5ae2e5d5e4d1a00ae731443cceb183e6900917b3
Moksleivis <Moksleivis@kitm.local>
2023-11-09 11:34:28 +0200

    wednesday

---
+++ pratybos/one/script.js
@@ -0,0 +1,58 @@
+
+let vardas = "Mantas"
+vardas ="Aidas"
+console.log(vardas)
+
+let x = 5
+console.log(x)
+console.log(++x)
+console.log(++x)
+
+let z = 9
+console.log(z)
+console.log(--z)
+console.log(--z)
+
+let s = "6"
+console.log(parseInt(s))
+console.log(isNaN(s))
+console.log(+s)
+console.log(isNaN(s))
+console.log(s*1)
+console.log(isNaN(s))
+
+
+let d = 15
+console.log(d)
+let suma = d/3
+console.log(suma)
+
+//Petras su Jonu
+
+let jonx = 80
+let jony = 1.76
+let jonkmi = jonx / jony **2
+let jon = jonkmi.toFixed(1)
+console.log('Jono kmi yra',jon)
+
+let petx = 60
+let pety = 1.82
+let petkmi = petx / pety **2
+let pet = petkmi.toFixed(1)
+console.log('Petro kmi yra',pet)
+
+console.log('Jono kmi yra disesnis')
+
+//trikampis
+
+let ta = 35
+let tb = 25
+let tc2 = ta**2 + tb**2
+let tc = Math.sqrt(tc2)
+console.log(tc)
+
+//laipsnei
+
+let cel = 21
+let far = (cel * (9 / 5)) + 32;
+console.log(far)
